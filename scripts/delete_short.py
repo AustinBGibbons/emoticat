f86f5c3786d4db896c018c26fbda4be26349089c
Austin <gibbons4@stanford.edu>
2013-06-19 13:47:48 -0400

    Creating a tsv for XDATA compatibility

--- scripts/delete_short.py
+++ scripts/delete_short.py
@@ -1,6 +1,7 @@
 import sys
 
 fd = open(sys.argv[1])
+of = open(sys.argv[2], 'w+')
 
 leng = -1
 
@@ -8,5 +9,6 @@ for line in fd :
   line = line.rstrip("\n").split(" ")
   if(leng < 0) : 
     leng = len(line)
-  if(len(line) == leng) : print(" ".join(line))
+  if(len(line) > leng) : print "Longer line after line 0 : ", line
+  if(len(line) == leng) : print >>of, (" ".join(line))
 
